a419d099eefe8a78cc8e45a66277ecc3df904550
claudioscalzo <claudio.scalzo@outlook.com>
2020-03-14 21:20:43 +0100

    Add GitHub buttons and description

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Rabe GitOps logo" src="./assets/logo.png">
+    <HelloWorld msg="Rabe GitOps"/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+
+// GitHub Buttons plugin
+import 'vue-github-buttons/dist/vue-github-buttons.css';
+import VueGitHubButtons from 'vue-github-buttons';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueGitHubButtons, { useCache: true });
 
 new Vue({
   render: (h) => h(App),
